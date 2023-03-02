d9d93ecd1cf5db7ac785406dd3954e5179d4fc73
spvn81 <spvn81@gmail.com>
2023-03-02 21:41:08 +0530

    chnages added

--- app/modules/admin/views/student-details/download.php
+++ app/modules/admin/views/student-details/download.php
@@ -20,10 +20,11 @@
 </style>
 
 <div class="certificate">
-<div style="margin-top: 395px;"><b style="margin-left: 330px;"><?= $model->student_name ?></b><b style="margin-left: 422px;" ><?= $model->class->class_name ?></b></div>
-<div style="margin-top: 40px;margin-left: 550px;"><b><?= $model->level ?></b></div>
-<div style="margin-top: 45px;margin-left: 290px;"><b><?= $model->school->school_name ?></b></div>
-<div style="margin-top: -20px;margin-left: 172px;"><b><?= $model->academic_year ?></b></div>
+<div style="margin-top: 395px;"><b style="margin-left: 330px;position:fixed"><?= $model->student_name ?>
+ </b><b style="margin-left: 816px;position:fixed" ><?= $model->class->class_name ?></b></div>
+<div style="margin-top: 62px;margin-left: 550px;position:fixed"><b><?= $model->level ?></b></div>
+<div style="margin-top: 122px;margin-left: 290px;position:fixed"><b><?= $model->school->school_name ?></b></div>
+<div style="margin-top: 124px;margin-left: 172px;position:fixed"><b><?= $model->academic_year ?></b></div>
 </div> 
 
 </body>
